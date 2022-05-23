29c26adb3a00ed637062fec470d1db2e2a05b9e1
Nelson Gou <nel4gou@gmail.com>
2022-05-23 07:24:03 -0700

    Add Slider breakpoints for responsiveness and fix CSS alignment

--- js/speakers.js
+++ js/speakers.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 30,
+    speed: 250,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1800: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        },
+        2500: {
+            slidesPerView: 5,
+            spaceBetween: 30
+        }
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
