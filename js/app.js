01ff6664547e42a6de1bf29a1daaf4b6bba70f03
Nelson Gou <nel4gou@gmail.com>
2022-05-23 18:46:38 -0700

    Move open tag to collapsible to simplify JS

--- js/app.js
+++ js/app.js
@@ -2,18 +2,14 @@ const hamburger = document.getElementById("hamburger")
 const navPages = document.getElementById("pages");
 const collapsibles = document.querySelectorAll(".collapsible");
 
-
 hamburger.addEventListener("click", function() {
     navPages.classList.toggle("show");
 });
 
 for (let i=0; i<collapsibles.length; i++) {
-    const collapsible = collapsibles[i];
-    const button = collapsible.children[0];
-    const symbol = button.children[1];
-    const content = collapsible.children[1];
+    const button = collapsibles[i].children[0];
     button.addEventListener("click", function() {
-        symbol.classList.toggle("open");
-        content.classList.toggle("open");
+        const collapsible = this.parentElement;
+        collapsible.classList.toggle("open")
     });
 }
