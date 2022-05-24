8ebeda15927864f0a0c0f76511d246edc74ac551
Nelson Gou <nel4gou@gmail.com>
2022-05-23 17:45:49 -0700

    Move collapsible JS to common

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+const hamburger = document.getElementById("hamburger")
 const navPages = document.getElementById("pages");
+const collapsibles = document.querySelectorAll(".collapsible");
 
-document.getElementById("hamburger").addEventListener("click", function() {
+
+hamburger.addEventListener("click", function() {
     navPages.classList.toggle("show");
 });
+
+for (let i=0; i<collapsibles.length; i++) {
+    const collapsible = collapsibles[i];
+    const button = collapsible.children[0];
+    const symbol = button.children[1];
+    const content = collapsible.children[1];
+    button.addEventListener("click", function() {
+        symbol.classList.toggle("open");
+        content.classList.toggle("open");
+    });
+}
