e99ab043c4e617b41a9cd7d40f7e9780a874b704
Nelson Gou <nel4gou@gmail.com>
2022-05-25 10:41:30 -0700

    Move rickroll to new file
    Use YouTube iframe API to try playing with audio and without so works on phones too (i think)

--- js/equilibrium.js
+++ js/equilibrium.js
@@ -1,5 +1,7 @@
 const msg = ["dont click ok", "you failed", "now u have to click", "thru all of this", "*sighs* :\("];
 const title = document.getElementById("title");
+const body = document.body;
+const favicon = document.querySelector("link[rel='shortcut icon']");
 
 title.addEventListener("click", function() {
     // troll messages
@@ -7,38 +9,13 @@ title.addEventListener("click", function() {
     for (let i=1; i<msg.length; i++)
         alert(msg[i]);
 
-    // url parameters: https://developers.google.com/youtube/player_parameters#Parameters
+    // create iframe for rickroll.html and append to body
+    const iframe = document.createElement("iframe");
+    iframe.src = "rickroll.html";
+    body.innerHTML = "";
+    body.appendChild(iframe);
 
-    document.body.innerHTML = `
-        <style>
-            body, div {
-                width: 100vw;
-                height: 100vh;
-            }
-
-            body {
-                background-color: black;
-                position: relative;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-            }
-
-            div {
-                position: absolute;
-                top: 0;
-                left: 0;
-                z-index: 100;
-            }
-
-            iframe {
-                width: 100%;
-                max-height: 100vh;
-                aspect-ratio: 16 / 9;
-                border: 0;
-            }
-        </style>
-        <div title="lol u can't stop rick"></div>
-        <iframe src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&playlist=dQw4w9WgXcQ&loop=1&disablekb=1&playsinline=1&iv_load_policy=3" allow="autoplay"></iframe>
-    `;
+    // change document title and favicon
+    document.title = "rickroll";
+    favicon.href = "img/rick.png";
 });
---
+++ js/rickroll.js
@@ -0,0 +1,46 @@
+// https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+
+// place youtube scripts as first scripts
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+        videoId: "dQw4w9WgXcQ",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters#Parameters
+            "autoplay": 1,
+            "controls": 0,
+            "iv_load_policy": 3,
+            "loop": 1,
+            "disablekb": 1,
+            "modestbranding": 1,
+            "playlist": "dQw4w9WgXcQ",
+            "playsinline": 1
+        },
+        events: {
+            "onStateChange": onPlayerStateChange
+        }
+    });
+}
+
+let tryWithAudio = false;
+function onPlayerStateChange(event) {
+    // try playing with audio first
+    if (!tryWithAudio) {
+        player.playVideo();
+        tryWithAudio = true;
+        console.log("try autoplay with audio");
+    }
+
+    // if does not succeed (try variable is true and not playing)
+    // then mute and try to play
+    if (tryWithAudio && event.data !== YT.PlayerState.PLAYING) {
+        player.mute();
+        player.playVideo();
+        console.log("try muted autoplay");
+    }
+}
