46e9d14fce16e6a0baefef1baed667e4765c56a6
Nelson Gou <nel4gou@gmail.com>
2022-09-12 21:47:30 -0700

    Add locale to HTML element

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="en">
         <Head>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         </Head>
